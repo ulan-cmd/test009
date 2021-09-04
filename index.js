cec74c0caf18b5037083ee4b258a7b1cd66020e8
ulancho <ulan.five@gmail.com>
2021-09-04 17:31:22 +0600

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+let f = document.querySelector('input[type=text]');
+
+ // f.onchange = function(){
+ // 	console.log('change: ', this.value);
+ // }
+
+// f.oninput = function(){
+// 	console.log(this.value);
+// }
+
+// f.onfocus = function(){
+// 	this.style.border = '1px solid red';
+// }
+
+// f.onblur = function(){
+// 	console.log('blur: ', this.value);
+// }
+
+// f.onselect = function(){
+// 	alert('Вы выделили текст');
+// }
+
+
